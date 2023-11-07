a3de5cc97b2077f019c23f4796c858499d00b5e7
Thomas Brüggemann <mail@thomasbrueggemann.com>
2023-11-07 18:48:02 +0100

    first try on detecting voice and transscribing in realtime

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 brew install ffmpeg
+
 /usr/bin/pip3 install -r requirements.txt
--- karl.py
+++ karl.py
@@ -1,7 +1,74 @@
 #!/usr/bin/env python3
 
-import whisper
+import whisper, os
+import numpy as np
+import sounddevice as sd
+from scipy.io.wavfile import write
 
-model = whisper.load_model("tiny")
-result = model.transcribe("audio.mp3")
-print(result["text"])
+Model = 'tiny'      # Whisper model size (tiny, base, small, medium, large)
+SampleRate = 44100  # Stream device recording frequency
+BlockSize = 30      # Block size in milliseconds
+Threshold = 0.1     # Minimum volume threshold to activate listening
+Vocals = [50, 1000] # Frequency range to detect sounds that could be speech
+EndBlocks = 40      # Number of blocks to wait before sending to Whisper
+
+class StreamHandler:
+    def __init__(self):
+
+        self.running = True
+        self.padding = 0
+        self.prevblock = self.buffer = np.zeros((0,1))
+        self.fileready = False
+        print("\033[96mLoading Whisper Model..\033[0m", end='', flush=True)
+        self.model = whisper.load_model(f'{Model}{".en"}')
+        print("\033[90m Done.\033[0m")
+
+    def callback(self, indata, frames, time, status):
+        if not any(indata):
+            print('\033[31m.\033[0m', end='', flush=True)
+            return
+        
+        freq = np.argmax(np.abs(np.fft.rfft(indata[:, 0]))) * SampleRate / frames
+        
+        if np.sqrt(np.mean(indata**2)) > Threshold and Vocals[0] <= freq <= Vocals[1]:
+            print('.', end='', flush=True)
+            if self.padding < 1: self.buffer = self.prevblock.copy()
+            self.buffer = np.concatenate((self.buffer, indata))
+            self.padding = EndBlocks
+        else:
+            self.padding -= 1
+            if self.padding > 1:
+                self.buffer = np.concatenate((self.buffer, indata))
+            elif self.padding < 1 < self.buffer.shape[0] > SampleRate: # if enough silence has passed, write to file.
+                self.fileready = True
+                write('buffer.wav', SampleRate, self.buffer) # TODO: figure out how to send data to Whisper directly..
+                self.buffer = np.zeros((0,1))
+            elif self.padding < 1 < self.buffer.shape[0] < SampleRate: # if recording not long enough, reset buffer.
+                self.buffer = np.zeros((0,1))
+                print("\033[2K\033[0G", end='', flush=True)
+            else:
+                self.prevblock = indata.copy() #np.concatenate((self.prevblock[-int(SampleRate/10):], indata)) # SLOW
+
+    def process(self):
+        if self.fileready:
+            print("\n\033[90mTranscribing..\033[0m")
+            result = self.model.transcribe('buffer.wav', fp16=False, language='en', task='transcribe')
+            print(f"\033[1A\033[2K\033[0G{result['text']}")
+            self.fileready = False
+
+    def listen(self):
+        print("\033[32mListening.. \033[37m(Ctrl+C to Quit)\033[0m")
+        with sd.InputStream(channels=1, callback=self.callback, blocksize=int(SampleRate * BlockSize / 1000), samplerate=SampleRate):
+            while self.running: self.process()
+
+def main():
+    try:
+        handler = StreamHandler()
+        handler.listen()
+    except (KeyboardInterrupt, SystemExit): pass
+    finally:
+        print("\n\033[93mQuitting..\033[0m")
+        if os.path.exists('buffer.wav'): os.remove('buffer.wav')
+
+if __name__ == '__main__':
+    main()
