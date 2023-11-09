55f1bf1dd9c692497e94e4e3ac90049d3824e846
Thomas Brüggemann <mail@thomasbrueggemann.com>
2023-11-09 20:02:32 +0100

    move to using openai gpt4 for conversations

--- src/brain.py
+++ src/brain.py
@@ -1,15 +1,26 @@
 
-import gpt4all
+import openai
 
 class Brain:
-    def __init__(self):
-        self.gpt_model = gpt4all.GPT4All("mistral-7b-openorca.Q4_0.gguf", "./")
+    def __init__(self, openai_api_key):
+        self.openai_client = openai.OpenAI(api_key=openai_api_key)
+        self.conversation_history = []
         
     def process(self, words):
         if words is None or len(words) == 0:
             return None
         
-        with self.gpt_model.chat_session(system_prompt="You are a little robot called 'Karl'"):
-            response = self.gpt_model.generate(prompt=words, temp=0)
+        self.conversation_history.append({
+            "role": "user",
+            "content": words,
+        })
             
-        return response
+        completion = self.openai_client.chat.completions.create(
+            model="gpt-4",
+            messages=self.conversation_history,
+            max_tokens=200
+        )
+
+        self.conversation_history.append(completion.choices[0].message)
+
+        return completion.choices[0].message.content
--- src/karl.py
+++ src/karl.py
@@ -1,12 +1,19 @@
 
+import sys
+
 from brain import Brain
 from ears import Ears
 from mouth import Mouth
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 karl.py <OPENAI_API_KEY>")
+        return
+
+    openai_api_key = sys.argv[1]
 
     ears = Ears()
-    brain = Brain()
+    brain = Brain(openai_api_key)
     mouth = Mouth()
 
     try:
